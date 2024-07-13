5eb4953b3b65dc7068a923809d608deb9cab5953
myatpwintphyu <myatpwintphyu.pk64@gmail.com>
2024-07-13 21:36:24 +0630

    writing js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const input = document.getElementById("text-input");
-const body= document.getElementById("body");
-const div = document.createElement ("div");
-body.append (div);
-input.addEventListener("click",()=>{
+const body = document.getElementById("body");
+const div = document.createElement("div");
+body.appendChild(div);
+
+input.addEventListener("click", () => {
     div.innerHTML = input.value;
-    console.log(div);
-})
+});
