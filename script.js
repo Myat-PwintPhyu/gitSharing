106fc64e2a5ad0643754889688bb1dd99e5e35e8
myatpwintphyu <myatpwintphyu.pk64@gmail.com>
2024-07-13 21:32:06 +0630

    created script file

--- script.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("text-input");
-const body= document.getElementById("body");
-const div = document.createElement ("div");
-body.append (div);
-input.addEventListener("click",()=>{
-    div.innerHTML = input.value;
-    console.log(div);
-})
